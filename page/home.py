8cbc1a0f8c5b2678ceb2e00b2f386356a1760f96
ByronLeeeee <demonbyron@gmail.com>
2025-01-15 15:11:34 +0800

    docs(home): 添加首页说明文字
    - 在首页添加了说明文字，包括模板搜索、填写、生成和下载的步骤说明
    - 增加了测试版的提示信息和联系方式
    - 优化了页面布局，添加了分割线

--- page/home.py
+++ page/home.py
@@ -10,4 +10,9 @@ pleading_type = st.selectbox("请选择你的文书类型：", PLEADING_PAGE_PAT
 case_type = st.selectbox("请选择你的案件类型：", CASE_CATEGORIES)
 
 if st.button("搜索模版",type='primary'):
-    st.page_link(f"page/{PLEADING_PAGE_PATH[pleading_type]}/{CASE_TYPE_PAGE_PATH[case_type]}",label="开始填写")   
+    st.page_link(f"page/{PLEADING_PAGE_PATH[pleading_type]}/{CASE_TYPE_PAGE_PATH[case_type]}",label="开始填写")
+
+
+st.markdown("_____")
+st.subheader("说明")
+st.markdown("1. 可以直接在侧边栏选择或搜索模版使用：选择文书类型和案件类型，点击`搜索模版`，再点击`开始填写`即可进入相应文书的填写页面。\n2. 根据模版内容填写完成后，点击`生成`按钮，数秒后即可生成完毕，然后点击`下载`按钮即可下载。\n3. 目前为测试版，可能有错漏之处，请尽量复查文件内容。\n4. 需要部署协助或有其他疑惑、建议，请联系微信`legal-lby`。")
